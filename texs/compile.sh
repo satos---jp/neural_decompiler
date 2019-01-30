ab967f4ea9105059cec10c9d3337eb9cf94f2397
satos---jp <syogo8118@yahoo.co.jp>
2019-01-30 10:55:48 +0900

    tozita

--- texs/compile.sh
+++ texs/compile.sh
@@ -1,4 +1,4 @@
 cp ../../tex-srcs/iscs-thesis.cls ./
 sed '/__NAMES__/r ../../names.txt' paper_base.tex | sed 's/__NAMES__//g' > paper.tex
-extractbb bleu.png && extractbb edit_dist.png && extractbb ast_lens.png && extractbb c_lens.png && extractbb ast_zu.png &&
+extractbb bleu.png && extractbb edit_dist.png && extractbb ast_lens.png && extractbb c_lens.png && extractbb ast_zu.png && extractbb encoder.png && extractbb decoder.png &&
 platex paper.tex && pbibtex paper && platex paper.tex && platex paper.tex && platex paper.tex && dvipdfm paper.dvi
